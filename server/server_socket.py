4a4d9656b4ff8de1bbdc348ae7159556bc82ade2
asrafulashiq <ashrafulislam@ash-mac.home>
2019-04-21 20:29:56 -0400

    //

--- server/server_socket.py
+++ server/server_socket.py
@@ -5,6 +5,7 @@ import io
 
 
 SERVER_PORT = 6800
+CHUNK = 1024
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('', SERVER_PORT))
@@ -12,17 +13,33 @@ server_socket.bind(('', SERVER_PORT))
 server_socket.listen()
 print("The server is ready to receive")
 
+conn, addr = server_socket.accept()
+im_bytes = []
+
 while True:
-    conn, addr = server_socket.accept()
-    data = conn.recv(1024)
+    data = conn.recv(CHUNK)
+
+    if data.decode("utf8").startswith("type"):
+        info = data.decode("utf8")
+        _type = info.split(":")[-1]
+        if _type == "image":
+            conn.sendall("ACK".decode("utf8"))
+    elif data.decode("utf8") == "BYE":
+        print("received all image bytes")
+        image = Image.open(io.BytesIO(im_bytes))
+        image.show()
+        break
+    else:
+        im_bytes += data
 
     # process data
-    image = Image.open(io.BytesIO(data))
-    image.show()
-    # send back data
-    proc_data = data
-    conn.send(proc_data)
+    # image = Image.open(io.BytesIO(data))
+    # image.show()
+    # # send back data
+    # proc_data = data
+    # conn.send(proc_data)
 
-    # close connection
-    conn.close()
+    # # close connection
+    # conn.close()
 
+conn.close()
--- vision/client_socket.py
+++ vision/client_socket.py
@@ -5,6 +5,7 @@ import socket
 SERVER_IP = '192.168.1.8'
 SERVER_PORT = 6800
 
+CHUNK = 1024
 
 def imfile_to_byte(imfile):
     with open(imfile, 'rb') as fp:
@@ -12,16 +13,33 @@ def imfile_to_byte(imfile):
         return im_data
 
 
+def send_data(client_socket, data, type="image"):
+    # initialize sending
+    init_str = f"type:{type}".encode('utf8')
+    client_socket.sendall(init_str)
+
+    # get confirmation
+    conf_dat = client_socket.recv(CHUNK)
+    if conf_dat.decode("utf-8") == "ACK":
+        client_socket.sendall(data)
+        client_socket.sendall("BYE".decode("utf8"))
+    else:
+        print("No confirmation!!!!")
+    # send actual file
+
+
+
 if __name__ == "__main__":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((SERVER_IP, SERVER_PORT))
 
-    imfile = '~/im.jpg'
+    imfile = '/home/pi/im.jpg'
     data = imfile_to_byte(imfile)
 
-    client.send(data)
+    # send image
+    send_data(client, data, type="image")
 
-    recv_data = client.recv(1024)
+    recv_data = client.recv(CHUNK)
     print('received data: ', recv_data)
 
     client.close()
