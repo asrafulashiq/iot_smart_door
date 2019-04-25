ac422ed3ae470ca2fdeaeb0f890d10f49648b119
asrafulashiq <ashrafulislam@ash-mac.home>
2019-04-25 00:37:40 -0400

    thread start

--- server/server_voice.py
+++ server/server_voice.py
@@ -5,6 +5,7 @@ import subprocess
 import time
 import utils
 import logging
+from voice_server_thread import VoiceServer
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,65 +20,9 @@ server_socket.listen()
 print("The server is ready to receive")
 
 conn, addr = server_socket.accept()
-logging.info("Connection accepted")
-
-
-def send_msg(msg, choice=None):
-    # start handshake
-    if msg is None:
-        msg = input("Write your message: ").strip()
-
-    stat = utils.start_handshake_send(conn)
-    if stat:
-        utils.send_data(conn, msg, choice=choice)
-
-
-dict_msg_template = {
-    "1": "Please come in",
-    "2": "Please leave the package outside front door",
-    "3": "Please call me this number: 123432908",
-    "4": "I am at work now. Can you please come tomorrow?",
-    "5": "[custom message]"
-}
-msg_template = sorted(dict_msg_template.items())
-
-my_templates = "What message do you want to send?\n"
-for i, info in msg_template:
-    my_templates += "   ({}) {}\n".format(i, info)
-my_templates += "->"
-
-choice_str = """
-What do you want to do?
-    (a) Send a message to the visitor
-    (b) Send a message to the visitor and then wait for response
-    (c) Ignore
-    (d) exit
-
-->"""
+logging.info("Connection established with {}".format(addr))
 
 while True:
-
-    my_choice = input(choice_str).lower()
-    logging.info("Your choice : {}".format(my_choice))
-
-    if my_choice in ('a', 'b'):
-        my_temp_choice = input(my_templates).lower().strip()
-        msg = dict_msg_template.get(my_temp_choice, None)
-        if my_temp_choice == '5':
-            msg = None
-        send_msg(msg, choice=my_choice)
-    
-        if my_choice == 'b':
-            logging.debug("Wait for receiving voice message")
-            data = utils.recv_data(conn)
-            print("------------")
-            print("Received: \n{}\t\n".format(data))
-            print("------------")
-
-    elif my_choice == 'd':
-        break
-    else:
-        pass
-
-
-conn.close()
+    voice_thread = VoiceServer(voice_socket=conn, log=logging)
+    voice_thread.start()
+    voice_thread.join()
---
+++ server/voice_server_thread.py
@@ -0,0 +1,63 @@
+import threading
+import utils
+
+dict_msg_template = {
+    "1": "Please come in",
+    "2": "Please leave the package outside front door",
+    "3": "Please call me this number: 123432908",
+    "4": "I am at work now. Can you please come tomorrow?",
+    "5": "[custom message]"
+}
+msg_template = sorted(dict_msg_template.items())
+
+my_templates = "What message do you want to send?\n"
+for i, info in msg_template:
+    my_templates += "   ({}) {}\n".format(i, info)
+my_templates += "->"
+
+choice_str = """
+What do you want to do?
+    (a) Send a message to the visitor
+    (b) Send a message to the visitor and then wait for response
+    (c) Ignore
+    (d) exit
+
+->"""
+
+
+def send_msg(conn, msg, choice=None):
+    # start handshake
+    if msg is None:
+        msg = input("Write your message: ").strip()
+
+    stat = utils.start_handshake_send(conn)
+    if stat:
+        utils.send_data(conn, msg, choice=choice)
+
+
+class VoiceServer(threading.Thread):
+    def __init__(self, voice_socket=None, log=None):
+        threading.Thread.__init__(self)
+        self.socket = voice_socket
+        self.log = log
+
+    def run(self):
+        my_choice = input(choice_str).lower()
+        self.log.info("Your choice : {}".format(my_choice))
+
+        if my_choice in ('a', 'b'):
+            my_temp_choice = input(my_templates).lower().strip()
+            msg = dict_msg_template.get(my_temp_choice, None)
+            if my_temp_choice == '5':
+                msg = None
+            send_msg(self.socket, msg, choice=my_choice)
+
+            if my_choice == 'b':
+                self.log.debug("Wait for receiving voice message")
+                data = utils.recv_data(self.socket)
+                print("------------")
+                print("Received: \n{}\t\n".format(data))
+                print("------------")
+        else:
+            pass
+
